3259771770852f4eb6960330a6e2305bca853fea
shamsundar2005 <shamsundarak2005@gmail.com>
2024-02-10 20:12:22 +0530

    updated weather

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href=""
+        > shamsundar</a
+      >, <a href="https://github.com/shamsundar2005/React-Weather-App">Open sourced on Github.</a> 
+    </p>
+  </div>
+  );
+}
